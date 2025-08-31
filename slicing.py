acd2c24f795e64bc16be116b95715cf78b65c738
sai srujana <srujananagireddy82@gmail.com>
2025-08-31 20:32:15 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,19 @@
+"""
+slicing in used to retrieve the elements from one particular range
+to another particular range
+the syntax: s^3(start:stop:skip)
+"""
+mylist=[ 1,2,13,15,16,17,18,19]
+print(mylist)
+print(mylist[0:6:2])
+
+
+mylist2=[2,5,6,7,8,9,10]
+print(mylist2[0:7:3])
+
+
+mylist3=[45,46,78,90,56,34,78,90,34,65,89]
+print(mylist3[0:8:4])
+
+mylist4=["hello",12,34,56,89.90,456+5j,4555,789]
+print(mylist4[2:6:2])
